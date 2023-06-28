ea1866cdb9085b11e3b99e3f8ec2cb6204781a33
BoutinL <boutin.loic@hotmail.com>
2023-06-28 09:51:58 +0200

    fin css basic

--- appli/index.php
+++ appli/index.php
@@ -3,32 +3,59 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Ajout produit</title>
 </head>
 <body>
-    <h1>Ajouter un produit</h1>
-    <form action="traitement.php" method="post">
-        <p>
-            <label>
-                Nom du produit :
-                <input type="text" name="name">
-            </label>
-        </p>
-        <p>
-            <label>
-                Prix du produit :
-                <input type="number" step="any" name="price">
-            </label>
-        </p>
-        <p>
-            <label>
-                Quantité désirée :
-                <input type="number" name="qtt" value="1">
-            </label>
-        </p>
-        <p>
-            <input type="submit" name="submit" value="Ajouter le produit">
-        </p>
-    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">MyFirstApp</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNav">
+                    <ul class="navbar-nav flex-navbar">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php">Ajout Produit</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="recap.php">Mon panier</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <h1>Ajouter un produit</h1>
+        <div class="form-group flex-form">
+            <form action="traitement.php" method="post">
+                <p>
+                    <label>
+                        Nom du produit :
+                        <input type="text"class="form-control" name="name">
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Prix du produit :
+                        <input type="number" class="form-control" step="any" name="price">
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Quantité désirée :
+                        <input type="number" class="form-control" name="qtt" value="1">
+                    </label>
+                </p>
+                <p>
+                    <input type="submit" class="btn btn-primary btn-form" name="submit" value="Ajouter le produit">
+                </p>
+            </form>
+        </div>
+    </main>
 </body>
 </html>
--- appli/recap.php
+++ appli/recap.php
@@ -6,42 +6,68 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Récapitulatif des produits</title>
 </head>
 <body>
-    <?php
-        if(!isset($_SESSION['products']) || empty($_SESSION['products'])){
-            echo "<p>Auncun produit en session...</p>";
-        }else{
-            echo"<table>",
-                    "<thead>",
-                        "<tr>",
-                            "<th>#</th>",
-                            "<th>Nom</th>",
-                            "<th>Prix</th>",
-                            "<th>Quantité</th>",
-                            "<th>Total</th>",
-                        "<tr/>",
-                    "</thead>",
-                    "<tbody>";
-            $totalGeneral = 0;
-            foreach($_SESSION['products'] as $index => $product){
-                    echo"<tr>",
-                            "<td>".$index."</td>",
-                            "<td>".$product['name']."</td>",
-                            "<td>".number_format($product['price'],2,",", "&nbsp;")."&nbsp;€</td>",
-                            "<td>".$product['qtt']."</td>",
-                            "<td>".number_format($product['total'],2,",", "&nbsp;")."&nbsp;€</td>",
-                        "</tr>";
-                        $totalGeneral += $product['total'];
+    <main>
+        <header>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">MyFirstApp</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNav">
+                        <ul class="navbar-nav flex-navbar">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="index.php">Ajout Produit</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="recap.php">Mon panier</a><span><?php count($_SESSION) ?></span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <section class="array-panier">
+            <?php
+                if(!isset($_SESSION['products']) || empty($_SESSION['products'])){
+                    echo "<p>Auncun produit en session...</p>";
+                }else{
+                    echo"<table class='table table-bordered'>",
+                            "<thead>",
+                                "<tr>",
+                                    "<th class='center-content-array'>#</th>",
+                                    "<th class='center-content-array'>Nom</th>",
+                                    "<th class='center-content-array'>Prix</th>",
+                                    "<th class='center-content-array'>Quantité</th>",
+                                    "<th class='center-content-array'>Total</th>",
+                                "<tr/>",
+                            "</thead>",
+                            "<tbody>";
+                    $totalGeneral = 0;
+                    foreach($_SESSION['products'] as $index => $product){
+                            echo"<tr>",
+                                    "<td class='center-content-array'>".$index."</td>",
+                                    "<td class='center-content-array'>".$product['name']."</td>",
+                                    "<td class='center-content-array'>".number_format($product['price'],2,",", "&nbsp;")."&nbsp;€</td>",
+                                    "<td class='center-content-array'>".$product['qtt']."</td>",
+                                    "<td class='center-content-array'>".number_format($product['total'],2,",", "&nbsp;")."&nbsp;€</td>",
+                                "</tr>";
+                                $totalGeneral += $product['total'];
+                        }
+                        echo    "<tr>",
+                                    "<td colspan=4>Total général : </td>",
+                                    "<td class='center-content-array'><strong>".number_format($totalGeneral, 2, ",", "&nbsp;")."&nbsp;€</strong></td>",
+                                "</tr>",
+                            "</tbody>",
+                        "</table>";
                 }
-                echo    "<tr>",
-                            "<td colspan=4>Total général : </td>",
-                            "<td><strong>".number_format($totalGeneral, 2, ",", "&nbsp;")."&nbsp;€</strong></td>",
-                        "</tr>",
-                    "</tbody>",
-                "</table>";
-        }
-    ?>
+            ?>
+        </section>
+    </main>
 </body>
 </html>
